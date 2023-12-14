d27f745e206fdd06aaafb99b390c67b78262ed1b
Ignasius <ignis19@protonmail.com>
2023-12-14 23:58:24 +0700

    comment out hacky wsl-clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,18 +8,18 @@ vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 
 -- clipboard
-vim.g.clipboard = {
-  name = "win32yank-wsl",
-  copy = {
-    ["+"] = "win32yank.exe -i --crlf",
-    ["*"] = "win32yank.exe -i --crlf",
-  },
-  paste = {
-    ["+"] = "win32yank.exe -o --lf",
-    ["*"] = "win32yank.exe -o --lf",
-  },
-  cache_enabled = true,
- }
+-- vim.g.clipboard = {
+--   name = "win32yank-wsl",
+--   copy = {
+--     ["+"] = "win32yank.exe -i --crlf",
+--     ["*"] = "win32yank.exe -i --crlf",
+--   },
+--   paste = {
+--     ["+"] = "win32yank.exe -o --lf",
+--     ["*"] = "win32yank.exe -o --lf",
+--   },
+--   cache_enabled = true,
+--  }
 
 -- indent
 vim.opt.expandtab = true -- replace tab with spaces
